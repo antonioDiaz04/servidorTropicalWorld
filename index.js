d442be7cf7cd4904e065bae4a532b265970c8b00
anthony= <134122438+Nikrovikey@users.noreply.github.com>
2024-03-26 05:47:56 -0600

    subiendo

--- index.js
+++ index.js
@@ -1,41 +1,35 @@
 const express = require('express');
 const conectarDB = require('./config/conexion');
 const cors = require('cors');
+
 // creamos el servidor
 const app = express();
 
 const bodyParser = require('body-parser');
-const cookieParser=require('cookie-parser');
+const cookieParser = require('cookie-parser');
+
 // conectamos a la base de datos
 conectarDB();
+
 const corsOptions = {
-    origin: 'https://tropicalworld.vercel.app/',  // Reemplaza con la URL de tu aplicación Angular
+    origin: 'https://tropicalworld.vercel.app',  // Reemplaza con la URL de tu aplicación Angular
     credentials: true,
-  };
-
-
+};
 
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
-app.use(express.json());
-app.use(cors())
 
-
-app.use('/correo',require('./routes/mensaje'))
-
-app.use('/comentarios',require('./routes/comentario'))
-app.use('/privado',require('./routes/privado'))
+// Rutas
+app.use('/correo', require('./routes/mensaje'));
+app.use('/comentarios', require('./routes/comentario'));
+app.use('/privado', require('./routes/privado'));
 app.use('/productos', require('./routes/producto'));
-app.use('/usuarios',require('./routes/usuario'));
-app.use('/dispositivos',require('./routes/dispositivo'));
+app.use('/usuarios', require('./routes/usuario'));
+app.use('/dispositivos', require('./routes/dispositivo'));
 
-
-
-
-
-
-
-app.listen(4000, () => {
-    console.log("el servidor esta corriendo perfectamente en el puerto 4000 ");
-})
+// Puerto de escucha
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log("El servidor está corriendo perfectamente en el puerto", PORT);
+});
