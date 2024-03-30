ebcfff78fd5b0300af5497282884ff2233200b58
anthony= <134122438+Nikrovikey@users.noreply.github.com>
2024-03-29 21:31:19 -0600

    servidor preguntas update v1

--- routes/privado.js
+++ routes/privado.js
@@ -7,7 +7,7 @@ const  privadoController=require('../controllers/privadoController')
 
 // pregunta
 router.put('/pregunta/:id', privadoController.actualizarPregunta);
-router.get('pregunta/:id', privadoController.obtenerPregunta);
+router.get('/pregunta/:id', privadoController.obtenerPregunta);
 router.delete('/eliminarPregunta/:id', privadoController.eliminarPregunta);
 // 
 
