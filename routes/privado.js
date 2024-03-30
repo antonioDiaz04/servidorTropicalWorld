0019b58ebdf438154d746eccade5641d1ec61235
anthony= <134122438+Nikrovikey@users.noreply.github.com>
2024-03-30 00:29:13 -0600

    servidor preguntas update v5

--- routes/privado.js
+++ routes/privado.js
@@ -12,7 +12,7 @@ router.delete('/eliminarPregunta/:id', privadoController.eliminarPregunta);
 // 
 
 // politicas
-router.put('/politica/:id', privadoController.actualizarPregunta);
+router.put('/politica/:id', privadoController.actualizarPolitica);
 router.get('/politica/:id', privadoController.obtenerPolitica);
 router.delete('/eliminarPolitica/:id', privadoController.eliminarPolitica);
 // 
